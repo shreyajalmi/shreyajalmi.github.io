840475812576306109c6faa2e393a52722d4992e
shreyajalmi <shreyajalmi@gmail.com>
2018-07-28 13:26:46 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(function () {
+  
+  var prog = $(".progress-bar");
+  
+prog.eq(0).animate({width:"60%"}, 2000)
+
+prog.eq(1).animate({width:"80%"}, 2000)
+
+prog.eq(2).animate({width:"70%"}, 2000)
+
+prog.eq(3).animate({width:"70%"}, 2000)
+
+prog.eq(4).animate({width:"60%"}, 2000)
+
+prog.eq(5).animate({width:"60%"}, 2000)
+
+prog.eq(6).animate({width:"50%"}, 2000)
+
+prog.eq(7).animate({width:"50%"}, 2000)
+
+prog.eq(8).animate({width:"60%"}, 2000)
+
+prog.eq(9).animate({width:"80%"}, 2000)
+
+prog.eq(10).animate({width:"70%"}, 2000)
+
+prog.eq(11).animate({width:"60%"}, 2000)
+
+
+});
